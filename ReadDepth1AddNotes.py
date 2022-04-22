12acaa2ebc508dbbac45e7af4fcd8c3073df36d9
jyoungjin <ccd1202@naver.com>
2022-04-22 17:36:13 +0900

    2022.04.22(금) position to postweld_heat_treatment 변경

--- ReadDepth1AddNotes.py
+++ ReadDepth1AddNotes.py
@@ -13,8 +13,8 @@ for(root, directories, files) in os.walk(dir_path):
             if exceptCompany not in file_path:
                 with open(file_path, 'r') as file:
                     jsonData = json.load(file)
-                if 'other' in jsonData['position']:
-                   jsonData['position']['others'] = jsonData['position']['other']
-                   del jsonData['position']['other']
+                if 'other' in jsonData['postweld_heat_treatment']:
+                   jsonData['postweld_heat_treatment']['others'] = jsonData['postweld_heat_treatment']['other']
+                   del jsonData['postweld_heat_treatment']['other']
                    with open(file_path, 'w', encoding='utf-8') as mk_f:
                        json.dump(jsonData, mk_f, indent=4, ensure_ascii=False)
