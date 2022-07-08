2758bb1cc0396f945a22495412d2ca48724c2320
jyoungjin <ccd1202@naver.com>
2022-07-08 17:31:10 +0900

    2022.07.08(금) 작업내역

--- Common/낙서장.py
+++ Common/낙서장.py
@@ -12,6 +12,7 @@ for(root, directories, files) in os.walk(dir_path):
             file_path = os.path.join(root, file)
             with open(file_path, 'r') as file:
                 jsonData = json.load(file)
-                if "singlepass_or_multipass" in jsonData['technique'] and "multipass_or_singlepass(per side)" in jsonData['technique']:
-                    print("Company: ", jsonData['pqr_info']['company'])
-                    print("PQR No: ", jsonData['pqr_info']['procedure_qualification_record_no'])
+            if 'tungsten_electrode_type' in jsonData['electrical_characteristics']:
+                if 'tungsten_electrode_size' not in jsonData['electrical_characteristics']:
+                    print(jsonData['pqr_info']['company'])
+                    print(jsonData['pqr_info']['original_file_name'])
--- JointDesign/DivideValueToMinMax.py
+++ JointDesign/DivideValueToMinMax.py
@@ -13,60 +13,74 @@ for(root, directories, files) in os.walk(dir_path):
                 jsonData = json.load(file)
 
                 # root_face
-                if 'root_face' in jsonData['joint_design']:
-                    value = jsonData['joint_design']['root_face'].upper().strip("MM")
-                    if "~" in value:
-                        arr = value.split('~')
-                        if float(arr[0]) > float(arr[1]):
-                            print("앞에 수가 더 큽니다.")
-                            print(jsonData['pqr_info']['procedure_qualification_record_no'])
-                        jsonData['joint_design']['_root_face_min(mm)']  = float(arr[0])
-                        jsonData['joint_design']['_root_face_max(mm)']  = float(arr[1])
-                    else:
-                        jsonData['joint_design']['_root_face_min(mm)'] = float(value)
-                        jsonData['joint_design']['_root_face_max(mm)'] = float(value)
-
-                # root_opening
-                if 'root_opening' in jsonData['joint_design']:
-                    value = jsonData['joint_design']['root_opening'].upper().strip("MM")
-                    if "~" in value:
-                        arr = value.split('~')
-                        if float(arr[0]) > float(arr[1]):
-                            print("앞에 수가 더 큽니다.")
-                            print(jsonData['pqr_info']['procedure_qualification_record_no'])
-                        jsonData['joint_design']['_root_opening_min(mm)'] = float(arr[0])
-                        jsonData['joint_design']['_root_opening_max(mm)'] = float(arr[1])
-                    else:
-                        jsonData['joint_design']['_root_opening_min(mm)'] = float(value)
-                        jsonData['joint_design']['_root_opening_max(mm)'] = float(value)
-
-                # groove_angle
-                if 'groove_angle' in jsonData['joint_design']:
-                    value = jsonData['joint_design']['groove_angle'].strip("°")
-                    if "~" in value:
-                        arr = value.split('~')
-                        if float(arr[0]) > float(arr[1]):
-                            print("앞에 수가 더 큽니다.")
-                            print(jsonData['pqr_info']['procedure_qualification_record_no'])
-                        jsonData['joint_design']['_groove_angle_min(°)'] = float(arr[0])
-                        jsonData['joint_design']['_groove_angle_max(°)'] = float(arr[1])
-                    else:
-                        jsonData['joint_design']['_groove_angle_min(°)'] = float(value)
-                        jsonData['joint_design']['_groove_angle_max(°)'] = float(value)
+                # if 'root_face' in jsonData['joint_design']:
+                #     value = jsonData['joint_design']['root_face'].upper().strip("MM")
+                #     if "~" in value:
+                #         arr = value.split('~')
+                #         if float(arr[0]) > float(arr[1]):
+                #             print("앞에 수가 더 큽니다.")
+                #             print(jsonData['pqr_info']['procedure_qualification_record_no'])
+                #         jsonData['joint_design']['_root_face_min(mm)']  = float(arr[0])
+                #         jsonData['joint_design']['_root_face_max(mm)']  = float(arr[1])
+                #     else:
+                #         jsonData['joint_design']['_root_face_min(mm)'] = float(value)
+                #         jsonData['joint_design']['_root_face_max(mm)'] = float(value)
+                #
+                # # root_opening
+                # if 'root_opening' in jsonData['joint_design']:
+                #     value = jsonData['joint_design']['root_opening'].upper().strip("MM")
+                #     if "~" in value:
+                #         arr = value.split('~')
+                #         if float(arr[0]) > float(arr[1]):
+                #             print("앞에 수가 더 큽니다.")
+                #             print(jsonData['pqr_info']['procedure_qualification_record_no'])
+                #         jsonData['joint_design']['_root_opening_min(mm)'] = float(arr[0])
+                #         jsonData['joint_design']['_root_opening_max(mm)'] = float(arr[1])
+                #     else:
+                #         jsonData['joint_design']['_root_opening_min(mm)'] = float(value)
+                #         jsonData['joint_design']['_root_opening_max(mm)'] = float(value)
+                #
+                # # groove_angle
+                # if 'groove_angle' in jsonData['joint_design']:
+                #     value = jsonData['joint_design']['groove_angle'].strip("°")
+                #     if "~" in value:
+                #         arr = value.split('~')
+                #         if float(arr[0]) > float(arr[1]):
+                #             print("앞에 수가 더 큽니다.")
+                #             print(jsonData['pqr_info']['procedure_qualification_record_no'])
+                #         jsonData['joint_design']['_groove_angle_min(°)'] = float(arr[0])
+                #         jsonData['joint_design']['_groove_angle_max(°)'] = float(arr[1])
+                #     else:
+                #         jsonData['joint_design']['_groove_angle_min(°)'] = float(value)
+                #         jsonData['joint_design']['_groove_angle_max(°)'] = float(value)
+                #
+                # # back_groove_angle
+                # if 'back_groove_angle' in jsonData['joint_design']:
+                #     value = jsonData['joint_design']['back_groove_angle'].strip("°")
+                #     if "~" in value:
+                #         arr = value.split('~')
+                #         if float(arr[0]) > float(arr[1]):
+                #             print("앞에 수가 더 큽니다.")
+                #             print(jsonData['pqr_info']['procedure_qualification_record_no'])
+                #         jsonData['joint_design']['_back_groove_angle_min(°)'] = float(arr[0])
+                #         jsonData['joint_design']['_back_groove_angle_max(°)'] = float(arr[1])
+                #     else:
+                #         jsonData['joint_design']['_back_groove_angle_min(°)'] = float(value)
+                #         jsonData['joint_design']['_back_groove_angle_max(°)'] = float(value)
 
                 # back_groove_angle
-                if 'back_groove_angle' in jsonData['joint_design']:
-                    value = jsonData['joint_design']['back_groove_angle'].strip("°")
+                if 'bevel_angle' in jsonData['joint_design']:
+                    value = jsonData['joint_design']['bevel_angle'].strip("°")
                     if "~" in value:
                         arr = value.split('~')
                         if float(arr[0]) > float(arr[1]):
                             print("앞에 수가 더 큽니다.")
                             print(jsonData['pqr_info']['procedure_qualification_record_no'])
-                        jsonData['joint_design']['_back_groove_angle_min(°)'] = float(arr[0])
-                        jsonData['joint_design']['_back_groove_angle_max(°)'] = float(arr[1])
+                        jsonData['joint_design']['_bevel_angle_min(°)'] = float(arr[0])
+                        jsonData['joint_design']['_bevel_angle_max(°)'] = float(arr[1])
                     else:
-                        jsonData['joint_design']['_back_groove_angle_min(°)'] = float(value)
-                        jsonData['joint_design']['_back_groove_angle_max(°)'] = float(value)
+                        jsonData['joint_design']['_bevel_angle_min(°)'] = float(value)
+                        jsonData['joint_design']['_bevel_angle_max(°)'] = float(value)
 
                 with open(file_path, 'w', encoding='utf-8') as mk_f:
                     json.dump(jsonData, mk_f, indent=2, ensure_ascii=False)
